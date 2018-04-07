e0a1bd94f8c01f27ac610c4c16b519cbf86ae972
hshah11 <harshendrashah812@gmail.com>
2018-04-07 14:57:09 +0530

    Initialization

--- app/src/main/java/com/shahharshendra/embeddedapps/MainActivity.java
+++ app/src/main/java/com/shahharshendra/embeddedapps/MainActivity.java
@@ -2,6 +2,15 @@ package com.shahharshendra.embeddedapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import javax.xml.transform.sax.SAXResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        final ArrayList<String> demoApps = new ArrayList<>();
+        demoApps.add("Currency Converter");
+        demoApps.add("Number Shapes");
+        demoApps.add("Higher or Lower");
+        demoApps.add("Basic Phrases");
+        demoApps.add("Times Table");
+        demoApps.add("Time Counter");
+        demoApps.add("Guess The Celebrity");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, demoApps);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, (position + 1) + " " + demoApps.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
