bc24c93a0629f77ea0729f6ca1d6bd795d8d3e35
hshah11 <harshendrashah812@gmail.com>
2018-04-08 03:37:58 +0530

    Added permissions

---
+++ app/src/main/java/com/shahharshendra/embeddedapps/GuessTheCelebrityActivity.java
@@ -0,0 +1,13 @@
+package com.shahharshendra.embeddedapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessTheCelebrityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_the_celebrity);
+    }
+}
